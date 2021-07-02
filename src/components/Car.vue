9d913f3dc583896670000b130ab46bdbd088dd82
Luis Miguel M.R <miguelluis912@gmail.com>
2021-07-01 20:25:07 -0500

    Primer commit del curso

---
+++ src/components/Car.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <p>Marca: {{ brand }}</p>
+    <!-- <p>
+      <span v-for="color in color"  :key="color"> {{ color }}</span>
+    </p> -->
+    <ul v-for="color in color"  :key="color">
+      <li>{{ color }}</li>
+    </ul>
+    <ul v-for="powers in powers" :key="powers">
+      <li v-if="powers > 50">{{ powers }}</li>
+    </ul>
+    <p>mensaje: {{ mensaje.text }}</p> 
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            brand: "nissan",
+            color: ['red','blue','green','yellow'],
+            mensaje: {
+                title: "hola mundo",
+                text: "gg"
+            },
+            powers: [10,120,30,40,50,60],
+        };
+    },
+
+    // setup() {
+    //      const d = "hsola";
+    //      const g = 1;
+        
+    //     return {
+    //         d,g,
+    //     }
+    // },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CarData.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <h1>hola mundo data</h1>
+    <p>Marca : {{ marca}}</p>
+    <p>Modelo: {{ modelo}}</p>
+    <p>Fuerza: {{ caballos}}</p>
+    <button @click="subir">Aumentar</button>
+    <button @click="disminuir">Disminuir</button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            marca: 'nisan',
+            modelo: 'mx',
+            caballos: 9,        
+            };
+    },
+    methods: {
+        subir() {
+            if(this.caballos <=10){
+
+                this.caballos = this.caballos + 1;
+            }
+        },
+        disminuir() {
+            if(this.caballos > 0){
+
+                this.caballos = this.caballos - 1;
+            }
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
